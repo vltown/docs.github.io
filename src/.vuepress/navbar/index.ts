779926f24d7bb65041647c0d96b95fd6ca276c78
xiaoccc <mm@rust.com>
2024-01-09 13:45:49 +0800

    Update

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -2,17 +2,17 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  {
-    text: "服务器",
-    icon: "study",
-    // prefix: "/规则/",
-    link: "docs",
-  },
+  // {
+  //   text: "服务器",
+  //   icon: "study",
+  //   // prefix: "/规则/",
+  //   link: "docs",
+  // },
   {
     text: "皮肤站",
     icon: "snow",
     // prefix: "/规则/",
-    link: "https://skin.vltown.com",
+    link: "https://skin.vltown.com/?lang=zh_CN",
   },
   {
     text: "卫星地图",
@@ -21,9 +21,9 @@ export const zhNavbar = navbar([
     link: "http://map.vltown.com",
   },
   {
-    text: "资源站",
+    text: "服务器宣传视频",
     icon: "install",
     // prefix: "/规则/",
-    link: "http://pan.vltown.com",
+    link: "https://www.bilibili.com/video/BV1uG411o7C9?t=3.2",
   }
 ]);
